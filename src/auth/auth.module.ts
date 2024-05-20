5310434afdf411e92fd577cf70df8da08d3a81dd
RimidalU <stankevichuuladimiru@gmail.com>
2024-05-20 21:09:14 +0300

    feat: implement local strategy and local-auth guard

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common'
+import { PassportModule } from '@nestjs/passport'
 
 import { AuthController } from './auth.controller'
 import { AuthService } from './auth.service'
 import { UserModule } from '@src/user'
+import { LocalStrategy } from './strategies'
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, PassportModule],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, LocalStrategy],
 })
 export class AuthModule {}
