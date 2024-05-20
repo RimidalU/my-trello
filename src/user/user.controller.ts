e2756b06a1162dd18faa07d8812685b553b962e6
RimidalU <stankevichuuladimiru@gmail.com>
2024-05-20 16:53:36 +0300

    feat: implement build-user-response user-controller method

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,21 @@
 import { Controller } from '@nestjs/common'
 import { ApiTags } from '@nestjs/swagger'
 import { UserService } from '@src/user/user.service'
+import { UserEntity } from './entities'
+import { UserItemDto } from './dto'
 
 @Controller('user')
 @ApiTags('User routes')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  private buildUserResponse(user: UserEntity): UserItemDto {
+    return {
+      itemId: user.id,
+      item: {
+        name: user.name,
+        email: user.email,
+      },
+    }
+  }
 }
