667a4f4688b70d555b24182cecdfe3ddea920962
RimidalU <stankevichuuladimiru@gmail.com>
2024-05-20 16:54:47 +0300

    feat: implement get-all user-controller method

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,22 @@
-import { Controller } from '@nestjs/common'
+import { Controller, Get } from '@nestjs/common'
 import { ApiTags } from '@nestjs/swagger'
 import { UserService } from '@src/user/user.service'
 import { UserEntity } from './entities'
-import { UserItemDto } from './dto'
+import { UserItemDto, UsersResponseDto } from './dto'
 
 @Controller('user')
 @ApiTags('User routes')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Get()
+  async getAll(): Promise<UsersResponseDto> {
+    const users = await this.userService.getAll()
+
+    return {
+      users: users.map((user) => this.buildUserResponse(user)),
+    }
+  }
 
   private buildUserResponse(user: UserEntity): UserItemDto {
     return {
