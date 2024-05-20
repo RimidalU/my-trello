172bf4444c15d0a2d30c62abeb96306c4b69a872
RimidalU <stankevichuuladimiru@gmail.com>
2024-05-20 14:43:49 +0300

    fix: remove lint error

--- src/database/migrations/1716192157249-migrations.ts
+++ src/database/migrations/1716192157249-migrations.ts
@@ -1,11 +1,8 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { MigrationInterface, QueryRunner } from 'typeorm'
 
 export class Migrations1716192157249 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {}
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {}
 }
