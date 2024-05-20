64f4e49709d6dd15c49e8a56f8624b8bad546aa2
RimidalU <stankevichuuladimiru@gmail.com>
2024-05-20 15:20:18 +0300

    doc: add user.controller to swagger

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,9 @@
 import { Controller } from '@nestjs/common'
+import { ApiTags } from '@nestjs/swagger'
 import { UserService } from '@src/user/user.service'
 
 @Controller('user')
+@ApiTags('User routes')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 }
