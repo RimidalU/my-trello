8dc99a7d6e86552ed1c72ba7e1f947286977a0cf
RimidalU <stankevichuuladimiru@gmail.com>
2024-05-20 21:15:05 +0300

    feat: add local-auth guard to login method

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common'
+import { Controller, Post, UseGuards } from '@nestjs/common'
+import { LocalAuthGuard } from './strategies'
+import { ApiTags } from '@nestjs/swagger'
 
+@ApiTags('Auth routes')
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  @UseGuards(LocalAuthGuard)
+  @Post('/login')
+  async login() {}
+}
