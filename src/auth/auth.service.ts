fdddfa5adc65f46a347c9010c4ba43a183acda18
RimidalU <stankevichuuladimiru@gmail.com>
2024-05-20 20:41:36 +0300

    feat: implement validate-user auth service method

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common'
+import * as bcrypt from 'bcrypt'
+
+import { UserService } from '@src/user'
+
+import { ValidateUserDto } from './dto'
+import { UserValidatedType } from './types/user-validated.type'
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private usersService: UserService) {}
+
+  async validateUser({
+    email,
+    password,
+  }: ValidateUserDto): Promise<UserValidatedType | null> {
+    const user = await this.usersService.getByEmail(email)
+    if (user && (await bcrypt.compare(password, user.password))) {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { password, ...result } = user
+      return result
+    }
+    return null
+  }
+}
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -10,5 +10,6 @@ import { UserEntity } from './entities'
   imports: [TypeOrmModule.forFeature([UserEntity])],
   providers: [UserService],
   controllers: [UserController],
+  exports: [UserService],
 })
 export class UserModule {}
