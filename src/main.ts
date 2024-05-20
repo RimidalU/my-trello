0404dd50ad973d2fdbd5a51d79322779e8555f3a
RimidalU <stankevichuuladimiru@gmail.com>
2024-05-20 15:00:18 +0300

    feat: add swagger to app

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { ConfigService } from '@nestjs/config'
+import { initSwagger } from 'app.swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  await app.listen(3000)
+
+  const config = await app.get(ConfigService)
+
+  const port = +config.get<number>('API_PORT') || 3000
+
+  initSwagger(app)
+
+  await app.listen(port)
 }
 bootstrap()
