fe0a51113ac67621898d488769c400fbf0c1a13a
RimidalU <stankevichuuladimiru@gmail.com>
2024-05-20 14:46:42 +0300

    test: add env service tests

--- src/env/env.service.spec.ts
+++ src/env/env.service.spec.ts
@@ -1,18 +1,41 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { EnvService } from './env.service';
+import { Test, TestingModule } from '@nestjs/testing'
+import { ConfigService } from '@nestjs/config'
+
+import { EnvService } from './env.service'
 
 describe('EnvService', () => {
-  let service: EnvService;
+  let envService: EnvService
+  let configService: ConfigService
+  const apiPort = 1111
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [EnvService],
-    }).compile();
+      providers: [
+        EnvService,
+        {
+          provide: ConfigService,
+          useValue: {
+            get: jest.fn().mockReturnValue(apiPort),
+          },
+        },
+      ],
+    }).compile()
 
-    service = module.get<EnvService>(EnvService);
-  });
+    envService = module.get<EnvService>(EnvService)
+    configService = module.get<ConfigService>(ConfigService)
+  })
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
-});
+  it('EnvService be defined', () => {
+    expect(envService).toBeDefined()
+  })
+
+  it('ConfigService should be defined', () => {
+    expect(configService).toBeDefined()
+  })
+
+  it('the env value should be returned', async () => {
+    expect(envService.get('API_PORT')).toEqual(apiPort)
+
+    expect(configService.get).toHaveBeenCalledWith('API_PORT', { infer: true })
+  })
+})
