ffd97eced256fc1ddb898ce82a830f35762e2669
RimidalU <stankevichuuladimiru@gmail.com>
2024-05-20 16:52:21 +0300

    feat: implement get-all user service method

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
+
 import { UserService } from './user.service'
 import { UserController } from './user.controller'
 
+import { UserEntity } from './entities'
+
 @Module({
+  imports: [TypeOrmModule.forFeature([UserEntity])],
   providers: [UserService],
   controllers: [UserController],
 })
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import { UserEntity } from './entities'
+import { Repository } from 'typeorm'
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  @InjectRepository(UserEntity)
+  private readonly userRepository: Repository<UserEntity>
+
+  async getAll(): Promise<UserEntity[]> {
+    return await this.userRepository.find()
+  }
+}
