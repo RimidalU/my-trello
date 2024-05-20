6459f768301baf157cd034c740d1222cdc37278e
RimidalU <stankevichuuladimiru@gmail.com>
2024-05-20 21:54:50 +0300

    feat: implement login auth controller method

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,23 @@
-import { Controller, Post, UseGuards } from '@nestjs/common'
+import { Body, Controller, Post, UseGuards, Request } from '@nestjs/common'
 import { LocalAuthGuard } from './strategies'
 import { ApiTags } from '@nestjs/swagger'
+import { AuthService } from './auth.service'
+
+import { LoginResponseDto, ValidateUserDto } from './dto'
+import { LoginSwaggerDecorator } from './decorators'
 
 @ApiTags('Auth routes')
 @Controller('auth')
 export class AuthController {
+  constructor(private authService: AuthService) {}
+
   @UseGuards(LocalAuthGuard)
   @Post('/login')
-  async login() {}
+  @LoginSwaggerDecorator()
+  async login(
+    @Body() createUserDto: ValidateUserDto,
+    @Request() req,
+  ): Promise<LoginResponseDto> {
+    return this.authService.login(req.user)
+  }
 }
