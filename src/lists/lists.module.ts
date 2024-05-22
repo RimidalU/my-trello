235e7516fe700ef6ea15cf07c5c346527aee0a2e
RimidalU <stankevichuuladimiru@gmail.com>
2024-05-23 00:52:12 +0300

    fix: add list and user entity to list module

--- src/lists/lists.module.ts
+++ src/lists/lists.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common'
 import { ListsService } from './lists.service'
 import { ListsController } from './lists.controller'
+import { UserEntity } from '@src/users/entities'
+import { ListEntity } from './entities'
+import { TypeOrmModule } from '@nestjs/typeorm'
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ListEntity, UserEntity])],
   providers: [ListsService],
   controllers: [ListsController],
 })
