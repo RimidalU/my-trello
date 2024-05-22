786847c32896808edf8b63d24edc1a9d8f155200
RimidalU <stankevichuuladimiru@gmail.com>
2024-05-23 00:51:32 +0300

    feat: implement create method in lists module

--- src/lists/lists.service.ts
+++ src/lists/lists.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import { UserEntity } from '@src/users/entities'
+import { Repository } from 'typeorm'
+import { ListEntity } from './entities'
+import { CreateListDto } from './dto'
 
 @Injectable()
-export class ListsService {}
+export class ListsService {
+  constructor(
+    @InjectRepository(ListEntity)
+    private readonly listRepository: Repository<ListEntity>,
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+  ) {}
+
+  async create(currentUserId: number, payload: CreateListDto): Promise<number> {
+    const owner = await this.userRepository.findOneBy({ id: currentUserId })
+
+    const newList = new ListEntity()
+    Object.assign(newList, { ...payload, user_id: owner })
+
+    const list = await this.listRepository.save(newList)
+
+    return list.id
+  }
+}
