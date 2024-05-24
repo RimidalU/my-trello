1359bd24440e5fbf9c938d532599d95ce00d287f
RimidalU <stankevichuuladimiru@gmail.com>
2024-05-24 07:43:52 +0300

    feat: implement build-list-response and get-all lists controller methods

--- src/lists/lists.controller.ts
+++ src/lists/lists.controller.ts
@@ -1,10 +1,11 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common'
+import { Body, Controller, Post, UseGuards, Get } from '@nestjs/common'
 import { ListsService } from './lists.service'
 import { JwtAuthGuard } from '@src/auth/strategies/jwt-auth.guard'
 import { UserInfo } from '@src/users/decorators'
-import { CreateListDto } from './dto'
+import { CreateListDto, ListItemDto, ListsResponseDto } from './dto'
 import { ListConfirmationResponseDto } from './dto'
-import { CreateSwaggerDecorator } from './decorators'
+import { CreateSwaggerDecorator, GetAllSwaggerDecorator } from './decorators'
+import { ListEntity } from './entities'
 
 @Controller('lists')
 export class ListsController {
@@ -22,12 +23,40 @@ export class ListsController {
     return this.buildListConfirmationResponse(listId)
   }
 
+  @UseGuards(JwtAuthGuard)
+  @Get()
+  @GetAllSwaggerDecorator()
+  async getAll(): Promise<ListsResponseDto> {
+    const lists = await this.listService.getAll()
+
+    return {
+      lists: lists.map((list) => this.buildListResponse(list)),
+    }
+  }
+
   private buildListConfirmationResponse(
     listId: number,
   ): ListConfirmationResponseDto {
     return {
       list: {
         itemId: listId,
+      },
+    }
+  }
+
+  private buildListResponse(list: ListEntity): ListItemDto {
+    return {
+      itemId: list.id,
+      item: {
+        name: list.name,
+        position: list.position,
+        createdAt: list.createdAt,
+      },
+      owner: {
+        id: list.owner.id,
+        name: list.owner.name,
+        email: list.owner.email,
+        createdAt: list.owner.createdAt,
       },
     }
   }
