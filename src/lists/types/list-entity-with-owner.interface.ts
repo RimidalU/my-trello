6cd1cfe304c02525e7acbc53c20fa80651c55936
RimidalU <stankevichuuladimiru@gmail.com>
2024-05-24 07:39:44 +0300

    fix: remove unused interface

--- src/lists/types/list-entity-with-owner.interface.ts
+++
@@ -1,6 +0,0 @@
-import { ListOwnerInfoDTO } from '../dto'
-import { ListEntity } from '../entities'
-
-export interface ListEntityWithOwnerInterface extends ListEntity {
-  owner: ListOwnerInfoDTO
-}
