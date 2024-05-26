e140f0064994dc6f2717684c3bf5150787292e92
RimidalU <stankevichuuladimiru@gmail.com>
2024-05-27 00:24:47 +0300

    feat: implement create card route

--- src/cards/cards.controller.ts
+++ src/cards/cards.controller.ts
@@ -1,4 +1,40 @@
-import { Controller } from '@nestjs/common'
+import { Body, Controller, Param, Post, UseGuards } from '@nestjs/common'
+import { ApiTags } from '@nestjs/swagger'
+import { CardsService } from './cards.service'
+import { JwtAuthGuard } from '@src/auth/strategies/jwt-auth.guard'
+import { UserInfo } from '@src/users/decorators'
+import { CardConfirmationResponseDto, CreateCardDto } from './dto'
+import { CreateSwaggerDecorator } from './decorators'
 
-@Controller('cards')
-export class CardsController {}
+@Controller('lists/:listId/cards')
+@ApiTags('Cards routes')
+export class CardsController {
+  constructor(private readonly cardsService: CardsService) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Post()
+  @CreateSwaggerDecorator()
+  async create(
+    @UserInfo('id') currentUserId: number,
+    @Param('listId') listId: number,
+    @Body() payload: CreateCardDto,
+  ): Promise<CardConfirmationResponseDto> {
+    const cardId = await this.cardsService.create(
+      currentUserId,
+      listId,
+      payload,
+    )
+
+    return this.buildCardConfirmationResponse(cardId)
+  }
+
+  private buildCardConfirmationResponse(
+    cardId: number,
+  ): CardConfirmationResponseDto {
+    return {
+      card: {
+        itemId: cardId,
+      },
+    }
+  }
+}
