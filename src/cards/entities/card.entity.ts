2e7c896534a7658bfc4305b5eae49e2915b8f478
RimidalU <stankevichuuladimiru@gmail.com>
2024-05-28 07:54:02 +0300

    feat: add cascade remove comments in cardentity

--- src/cards/entities/card.entity.ts
+++ src/cards/entities/card.entity.ts
@@ -30,7 +30,9 @@ export class CardEntity {
   public owner: UserEntity
 
   @JoinColumn({ name: 'comments' })
-  @OneToMany(() => CommentEntity, (comment) => comment.card, { eager: true })
+  @OneToMany(() => CommentEntity, (comment) => comment.card, {
+    cascade: ['remove'],
+  })
   public comments: CommentEntity[]
 
   @ManyToOne(() => ListEntity, (list) => list.cards)
