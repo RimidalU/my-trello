e1da1db96fa118053e16c4123c8a0c574b0f26a9
RimidalU <stankevichuuladimiru@gmail.com>
2024-06-03 01:15:28 +0300

    test: implement base cards module test

--- src/cards/cards.controller.spec.ts
+++ src/cards/cards.controller.spec.ts
@@ -1,18 +1,31 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { CardsController } from './cards.controller'
+import { CardsService } from './cards.service'
 
 describe('CardsController', () => {
   let controller: CardsController
+  let service: CardsService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CardsController],
+      providers: [
+        {
+          provide: CardsService,
+          useValue: {},
+        },
+      ],
     }).compile()
 
     controller = module.get<CardsController>(CardsController)
+    service = module.get(CardsService)
   })
 
-  it('should be defined', () => {
+  it('controller should be defined', () => {
     expect(controller).toBeDefined()
+  })
+
+  it('service should be defined', () => {
+    expect(service).toBeDefined()
   })
 })
--- src/cards/cards.service.spec.ts
+++ src/cards/cards.service.spec.ts
@@ -1,18 +1,59 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { CardsService } from './cards.service'
+import { Repository } from 'typeorm'
+import { UserEntity } from '@src/users/entities'
+import { CardEntity } from './entities'
+import { getRepositoryToken } from '@nestjs/typeorm'
+import { ListEntity } from '@src/lists/entities'
 
 describe('CardsService', () => {
   let service: CardsService
+  let userRepository: Repository<UserEntity>
+  let cardRepository: Repository<CardEntity>
+  let listRepository: Repository<ListEntity>
+
+  const CARD_REPOSITORY_TOKEN = getRepositoryToken(CardEntity)
+  const USER_REPOSITORY_TOKEN = getRepositoryToken(UserEntity)
+  const LIST_REPOSITORY_TOKEN = getRepositoryToken(ListEntity)
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CardsService],
+      providers: [
+        CardsService,
+        {
+          provide: CARD_REPOSITORY_TOKEN,
+          useValue: {},
+        },
+        {
+          provide: USER_REPOSITORY_TOKEN,
+          useValue: {},
+        },
+        {
+          provide: LIST_REPOSITORY_TOKEN,
+          useValue: {},
+        },
+      ],
     }).compile()
 
     service = module.get<CardsService>(CardsService)
+    userRepository = module.get(USER_REPOSITORY_TOKEN)
+    cardRepository = module.get(CARD_REPOSITORY_TOKEN)
+    listRepository = module.get(LIST_REPOSITORY_TOKEN)
   })
 
   it('should be defined', () => {
     expect(service).toBeDefined()
+  })
+
+  it('should userRepository be defined', () => {
+    expect(userRepository).toBeDefined()
+  })
+
+  it('should cardRepository be defined', () => {
+    expect(cardRepository).toBeDefined()
+  })
+
+  it('should cardRepository be defined', () => {
+    expect(listRepository).toBeDefined()
   })
 })
