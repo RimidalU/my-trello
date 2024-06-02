5b84c1f672f139350a9159f79c89909f50a3fd97
RimidalU <stankevichuuladimiru@gmail.com>
2024-06-03 01:15:05 +0300

    test: implement base auth module test

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,18 +1,34 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { AuthController } from './auth.controller'
+import { AuthService } from './auth.service'
+import { userItem } from '@src/users/mocks'
 
 describe('AuthController', () => {
   let controller: AuthController
+  let service: AuthService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
+      providers: [
+        {
+          provide: AuthService,
+          useValue: {
+            validateUser: jest.fn().mockReturnValue(userItem),
+          },
+        },
+      ],
     }).compile()
 
-    controller = module.get<AuthController>(AuthController)
+    controller = module.get(AuthController)
+    service = module.get(AuthService)
   })
 
-  it('should be defined', () => {
+  it('controller should be defined', () => {
     expect(controller).toBeDefined()
+  })
+
+  it('service should be defined', () => {
+    expect(service).toBeDefined()
   })
 })
--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,18 +1,51 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { AuthService } from './auth.service'
+import { JwtService } from '@nestjs/jwt'
+import { UserService } from '@src/users'
+import { userItem } from '@src/users/mocks'
 
 describe('AuthService', () => {
   let service: AuthService
+  let userService: UserService
+  let jwtService: JwtService
+  const mockJwtToken =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MzYsIm5hbWUiOiJVc2VyIE5hbWUiLCJlbWFpbCI6InVzZXJAZWRkbWFpbC5jb20iLCJpYXQiOjE3MTE5NjA4NjcsImV4cCI6MTcxMTk2MDkyN30.8Kqn3HGIkTdUKlluAyvGydTw2azL22trL6lzwFWtjO4'
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        {
+          provide: UserService,
+          useValue: {
+            getByEmail: jest.fn().mockReturnValue(userItem),
+          },
+        },
+        JwtService,
+        {
+          provide: JwtService,
+          useValue: {
+            secret: process.env.JWT_CONSTANTS_SECRET,
+            sign: jest.fn().mockReturnValue(mockJwtToken),
+          },
+        },
+      ],
     }).compile()
 
     service = module.get<AuthService>(AuthService)
+    userService = module.get(UserService)
+    jwtService = module.get(JwtService)
   })
 
-  it('should be defined', () => {
+  it('AuthService should be defined', () => {
     expect(service).toBeDefined()
+  })
+
+  it('UserService should be defined', () => {
+    expect(userService).toBeDefined()
+  })
+
+  it('jwtService should be defined', () => {
+    expect(jwtService).toBeDefined()
   })
 })
